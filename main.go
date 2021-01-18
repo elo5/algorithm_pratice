ab80b6c922676e991449f9225c8fd1fcd0fb85fd
elo5 <elo5@qq.com>
2021-01-18 22:23:23 +0800

    thought i  solved

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main()  {
 	//fmt.Println(easy.IsPalindrome(123321))
 	//fmt.Println(easy.TwoSum([]int{1,7,2,15},9))
 	//fmt.Println(easy.TwoSum_faster([]int{3,5,4,2},6))
-	fmt.Println(easy.TwoSum_faster([]int{2,4,7,9,8},12))
-	fmt.Println(easy.TwoSum([]int{2,4,7,9,8},12))
+	//fmt.Println(easy.TwoSum_faster([]int{2,4,7,9,8},12))
+	//fmt.Println(easy.TwoSum([]int{2,4,7,9,8},12))
+
+	fmt.Println(easy.RemoveDuplicates([]int{1,1,2}))
+	//fmt.Println(easy.RemoveDuplicates([]int{1,2,2,3,4,5,5}))
 }
