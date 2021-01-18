d4a239cb0a351ee804650ce82b9576b2632ffb1b
elo <elo5@qq.com>
2021-01-18 11:58:46 +0800

    complete two sum

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main()  {
 	//fmt.Println("abc")
 
 	//fmt.Println(easy.RevverseInteger_COOLFUN(-1234567))
-	fmt.Println(easy.IsPalindrome(123321))
-
+	//fmt.Println(easy.IsPalindrome(123321))
+	//fmt.Println(easy.TwoSum([]int{1,7,2,15},9))
+	fmt.Println(easy.TwoSum([]int{3,5,4,2},6))
 }
