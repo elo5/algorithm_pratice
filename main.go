0cc2c8930777317b1d23c0186e599351cd11599a
elo <elo5@qq.com>
2021-01-19 16:05:35 +0800

    search insert position solved

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ func main()  {
 	//fmt.Println(easy.TwoSum_faster([]int{2,4,7,9,8},12))
 	//fmt.Println(easy.TwoSum([]int{2,4,7,9,8},12))
 
-	fmt.Println(easy.RemoveDuplicates([]int{1,1,2}))
+	//fmt.Println(easy.RemoveDuplicates([]int{1,1,2}))
 	//fmt.Println(easy.RemoveDuplicates([]int{0,0,1,1,1,2,2,3,3,4}))
 	//fmt.Println(easy.RemoveDuplicates([]int{1,2,2,3,4,5,5}))
+
+	fmt.Println(easy.SearchInsert( []int{2},1  ))
 }
